56722ae127ab83a6fc19f1d5eeffa6c4c95d547f
xwinkX <opalyuk.e88@ukr.net>
2022-08-05 21:57:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,7 @@ import Filter from './Filter/Filter';
 class App extends React.Component {
 
 state = {
-  contacts:
- [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter:''
 }
 
@@ -34,7 +28,21 @@ state = {
       }));
     }
   };
-
+  
+componentDidUpdate(prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+  console.log('sss')
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  };     
+};
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    };
+  };
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
   };
@@ -47,11 +55,11 @@ state = {
     );
   };
 
-  deleteContacts = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id)
-    }));
-  };
+    deleteContacts = id => {
+      this.setState(prevState => ({
+        contacts: prevState.contacts.filter(contact => contact.id !== id)
+      }));
+    };
 
 
   render() {
